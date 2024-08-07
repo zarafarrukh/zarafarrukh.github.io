559adac87f1f465b080fce713d44b345a95cbee6
Zara Farrukh <zara.farrukh@ontariotechu.net>
2024-08-07 12:33:59 +0300

    contact: removed phone prompt

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,6 @@ export const Contact = () => {
     firstName: '',
     lastName: '',
     email: '',
-    phone: '',
     message: ''
   }
   const [formDetails, setFormDetails] = useState(formInitialDetails);
@@ -69,9 +68,6 @@ export const Contact = () => {
                     </Col>
                     <Col size={12} sm={6} className="px-1">
                       <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
                     </Col>
                     <Col size={12} className="px-1">
                       <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
